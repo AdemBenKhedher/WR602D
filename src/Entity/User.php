340c25299d0cab42be23f30dd03810cac75c1655
AdemBenKhedher <adem.ben-khedher@etudiant.univ-reims.fr>
2025-02-28 09:38:02 +0100

    fix: Remove unnecessary blank line in User entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -34,7 +34,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     #[ORM\Column]
     private ?string $password = null;
-    
+
     #[ORM\Column(length: 255)]
     private ?string $lastname = null;
 
