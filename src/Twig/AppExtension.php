8e4cb31e72a3789e972a0802a0a4b8e72376eb5e
AdemBenKhedher <adem.ben-khedher@etudiant.univ-reims.fr>
2025-03-17 17:22:51 +0100

    feat: Implement AccountController and user account template | Enhance registration form with improved styling and additional links for all pages

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -5,6 +5,8 @@ namespace App\Twig;
 use Twig\Extension\AbstractExtension;
 use Twig\Extension\GlobalsInterface;
 use Symfony\Bundle\SecurityBundle\Security;
+use Doctrine\Persistence\ManagerRegistry;
+use App\Entity\Subscription;
 use App\Repository\SubscriptionRepository;
 use App\Repository\FileRepository;
 use DateTimeImmutable;
@@ -14,15 +16,18 @@ class AppExtension extends AbstractExtension implements GlobalsInterface
     private Security $security;
     private SubscriptionRepository $subscriptionRepo;
     private FileRepository $fileRepository;
+    private ManagerRegistry $doctrine;
 
     public function __construct(
         Security $security,
         SubscriptionRepository $subscriptionRepo,
+        ManagerRegistry $doctrine,
         FileRepository $fileRepository
     ) {
         $this->security = $security;
         $this->subscriptionRepo = $subscriptionRepo;
         $this->fileRepository = $fileRepository;
+        $this->doctrine = $doctrine;
     }
 
     public function getGlobals(): array
@@ -38,10 +43,11 @@ class AppExtension extends AbstractExtension implements GlobalsInterface
                 $endOfDay
             )
             : 0;
-
+        $subscriptions = $this->doctrine->getRepository(Subscription::class)->findAll();
         return [
             'user_subscription' => $userSubscription,
             'count' => $pdfCount,
+            'subscriptions' => $subscriptions,
         ];
     }
 }
