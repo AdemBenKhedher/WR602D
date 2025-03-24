80f80c3a237195fcad7ab113b0d63bb61cb6dc85
AdemBenKhedher <adem.ben-khedher@etudiant.univ-reims.fr>
2025-03-24 22:24:57 +0100

    feat: Implement reset password functionality | Add reset password request form, email template, and repository

---
+++ src/Controller/ResetPasswordController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\ChangePasswordFormType;
+use App\Form\ResetPasswordRequestFormType;
+use App\Service\ResetPasswordService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
+use SymfonyCasts\Bundle\ResetPassword\Controller\ResetPasswordControllerTrait;
+use SymfonyCasts\Bundle\ResetPassword\Model\ResetPasswordToken;
+
+#[Route('/reset-password')]
+class ResetPasswordController extends AbstractController
+{
+    use ResetPasswordControllerTrait;
+
+    private ResetPasswordService $resetPasswordService;
+
+    public function __construct(ResetPasswordService $resetPasswordService)
+    {
+        $this->resetPasswordService = $resetPasswordService;
+    }
+
+    #[Route('', name: 'app_forgot_password_request')]
+    public function request(Request $request): Response
+    {
+        $form = $this->createForm(ResetPasswordRequestFormType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $email = $form->get('email')->getData();
+            $resetToken = $this->resetPasswordService->processPasswordResetRequest($email);
+
+            if ($resetToken instanceof ResetPasswordToken) {
+                $this->setTokenObjectInSession($resetToken);
+            }
+
+            return $this->redirectToRoute('app_check_email');
+        }
+
+        return $this->render('reset_password/request.html.twig', [
+            'requestForm' => $form,
+        ]);
+    }
+
+    #[Route('/check-email', name: 'app_check_email')]
+    public function checkEmail(): Response
+    {
+        $resetToken = $this->getTokenObjectFromSession();
+        if (null === $resetToken) {
+            $resetToken = $this->resetPasswordService->generateFakeResetToken();
+        }
+
+        return $this->render('reset_password/check_email.html.twig', [
+            'resetToken' => $resetToken,
+        ]);
+    }
+
+    #[Route('/reset/{token}', name: 'app_reset_password')]
+    public function reset(
+        Request $request,
+        UserPasswordHasherInterface $passwordHasher,
+        TranslatorInterface $translator,
+        ?string $token = null
+    ): Response {
+        if ($token) {
+            $this->storeTokenInSession($token);
+            return $this->redirectToRoute('app_reset_password');
+        }
+
+        $token = $this->getTokenFromSession();
+        if (null === $token) {
+            throw $this->createNotFoundException('No reset password token found in the URL or in the session.');
+        }
+
+        $user = $this->resetPasswordService->validateTokenAndFetchUser($token, $translator);
+        if (!$user) {
+            return $this->redirectToRoute('app_forgot_password_request');
+        }
+
+        $form = $this->createForm(ChangePasswordFormType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $plainPassword = $form->get('plainPassword')->getData();
+            $this->resetPasswordService->updateUserPassword($user, $plainPassword, $passwordHasher);
+            $this->cleanSessionAfterReset();
+
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('reset_password/reset.html.twig', [
+            'resetForm' => $form,
+        ]);
+    }
+}
--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -14,6 +14,10 @@ class SecurityController extends AbstractController
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
+        $user = $this->getUser();
+        if ($user) {
+            return $this->redirectToRoute('app_Home');
+        }
         $error = $authenticationUtils->getLastAuthenticationError();
 
         $lastUsername = $authenticationUtils->getLastUsername();
