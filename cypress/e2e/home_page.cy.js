086587ba5abebba8c900a5eea38bac21ac3f3ac5
AdemBenKhedher <adem.ben-khedher@etudiant.univ-reims.fr>
2025-03-27 17:14:44 +0100

    fix: Update Qualitycheck workflow and home page test | Remove Symfony server start command and change visit URL to root for accurate testing

--- cypress/e2e/home_page.cy.js
+++ cypress/e2e/home_page.cy.js
@@ -1,6 +1,6 @@
 describe('Page d\'accueil', () => {
   it('Visite la page d\'accueil', () => {
-    cy.visit('http://symfony.mmi-troyes.fr:8319/')
+    cy.visit('/')
     cy.contains('h1', 'Bienvenue').should('be.visible')
   })
 })
