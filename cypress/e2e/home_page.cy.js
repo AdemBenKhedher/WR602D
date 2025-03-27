1f6c3c9271c0164872361a5c274c0a4cb1e09f74
AdemBenKhedher <adem.ben-khedher@etudiant.univ-reims.fr>
2025-03-27 16:46:34 +0100

    fix: Update home page test URL | Change visit URL to specific endpoint for accurate testing

--- cypress/e2e/home_page.cy.js
+++ cypress/e2e/home_page.cy.js
@@ -1,6 +1,6 @@
 describe('Page d\'accueil', () => {
   it('Visite la page d\'accueil', () => {
-    cy.visit('/')
+    cy.visit('http://symfony.mmi-troyes.fr:8319/')
     cy.contains('h1', 'Bienvenue').should('be.visible')
   })
 })
