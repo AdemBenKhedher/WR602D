ac096d7919d78913eacb3e00a5f809bf18120dfd
AdemBenKhedher <adem.ben-khedher@etudiant.univ-reims.fr>
2025-03-27 16:41:27 +0100

    fix: Update Cypress configuration | Set baseUrl to null for improved flexibility

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://symfony.mmi-troyes.fr:8319',
+    baseUrl: null,
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     supportFile: 'cypress/support/e2e.js',
     setupNodeEvents(on, config) {
